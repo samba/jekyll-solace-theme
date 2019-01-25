90a9fc8f19e0178394da3aec04f7f878eaa4b04b
Sam Briesemeister <sam.briesemeister@gmail.com>
2019-01-25 01:07:06 -0800

    Preliminary category and tag indexes

--- assets/site.js
+++ assets/site.js
@@ -26,4 +26,44 @@ layout: null
     mq('(max-width: 40em)', mq_report('compressed'));
 
 
+    function applyHeader(text){
+        var main = $('header h1');
+        if(text === null){
+            text = main.attr('data-default-text');
+        }
+        if(! main.attr('data-default-text')){
+            main.attr('data-default-text', main.text());
+        }
+        main.text(text);
+    }
+
+    function toggleTagSections(){
+        if($(document.body).hasClass('tag-listing')){
+            var anchor = window.location.hash.substr(1);
+            var active = 'section.tag-listing[data-tag-slug="' + anchor + '"]';
+            if(anchor){
+                $('section.tag-listing').hide();
+                $(active).show();
+                $(active).find('h2').first().each(function(){
+                    applyHeader($(this).text());
+                    $(this).hide();
+                });
+            } else {
+                applyHeader(null);
+            }
+        }
+    }
+
+
+    $(document).ready(function(){
+        $(window).on('hashchange', function(){
+            toggleTagSections();
+        });
+
+        if(window.location.hash){
+            $(window).trigger('hashchange');
+        }
+
+    });
+
 }(window.jQuery, window));
